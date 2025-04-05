039c8b7c93236acd5b0684eb482a5904bf96a2a2
iagobarross <iago.demeo@fatec.sp.gov.br>
2025-04-05 19:34:01 -0300

    Primeiro commit

---
+++ src/test/java/br/teste/minha_primeira_api_restful/MinhaPrimeiraApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.teste.minha_primeira_api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaPrimeiraApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
