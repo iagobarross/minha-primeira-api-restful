8a9bf2efc0549249efaa69f21151973279315792
iagobarross <iago.demeo@fatec.sp.gov.br>
2025-04-06 00:27:28 -0300

    Implementação da camada de domínio

--- src/main/java/br/teste/Application.java
+++ src/main/java/br/teste/Application.java
@@ -1,13 +1,13 @@
-package br.teste.minha_primeira_api_restful;
+package br.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MinhaPrimeiraApiRestfulApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MinhaPrimeiraApiRestfulApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
